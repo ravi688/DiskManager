bae26ecae80a0226905dee17efad6b346c3ceb8c
ravi688 <rp0412204@gmail.com>
2021-12-05 03:08:13 +0530

    Fixed load_binary_from_file and updated submodules

--- source/main.c
+++ source/main.c
@@ -8,10 +8,13 @@ int main(int argc, char** argv)
 	// BUFFER* data = load_text_from_file("makefile");
 	// puts(data->bytes);
 	// buf_free(data);
+	BUFFER* binary_data = load_binary_from_file("Binary-Sphere.stl");
+	printf("Binary length: %u\n", binary_data->element_count);
 
 	BUFFER* another_data = load_text_from_file("DiskManager.gv");
 	puts(another_data->bytes);	
 	write_text_to_file("AnotherDiskManager.txt", another_data->bytes);
 	buf_free(another_data);
+	buf_free(binary_data);
 	return 0;
 }
